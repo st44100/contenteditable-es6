e5d1e9318559fa30beaf84253daab820cc6c74d0
Hiroyuki SATAKE <satake_hiroyuki@cyberagent.co.jp>
2015-05-05 14:17:22 +0900

    Add: update event

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -3,57 +3,104 @@ import rangy from 'rangy'
 
 console.log(rangy);
 
+function extend(dest, src) {
+  Object.keys(src).forEach((k) => {
+    dest[k] = src[k];
+  });
+  return dest;
+}
+
 export class Editor {
 
   constructor(id = null, options = {}) {
     this.id = id;
     this.editor = null;
     this.empty = true;
+    this.value = '';
     this.options = {};
     let baseOptions = {
-      placeholder: 'Type Something...'
+      placeholder: 'Type Something...',
+      buttons:[
+        {
+          key: 'h2',
+          html: '<button class="btn btn-default">H2</button>',
+          action: (e) => this.onButtonAction(e, 'formatBlock', false, '<h2>')
+        },
+        {
+          key: 'h3',
+          html: '<button class="btn btn-default">H3</button>',
+          action: (e) => this.onButtonAction(e, 'formatBlock', false, '<h3>')
+        },
+        {
+          key: 'h4',
+          html: '<button class="btn btn-default">H4</button>',
+          action: (e) => this.onButtonAction(e, 'formatBlock', false, '<h4>')
+        },
+        {
+          key: 'h5',
+          html: '<button class="btn btn-default">H5</button>',
+          action: (e) => this.onButtonAction(e, 'formatBlock', false, '<h5>')
+        },
+        {
+          key: 'bold',
+          html: '<button class="btn btn-default"><b>B</b></button>',
+          action: (e) => this.onButtonAction(e, 'bold', false, null)
+        },
+        {
+          key: 'italic',
+          html: '<button class="btn btn-default"><i>I</i></button>',
+          action: (e) => this.onButtonAction(e, 'italic', false, null)
+        },
+        {
+          key: 'underline',
+          html: '<button class="btn btn-default"><u>U</u></button>',
+          action: (e) => this.onButtonAction(e, 'underline', false, null)
+        },
+        {
+          key: 'strikethrough',
+          html: '<button class="btn btn-default"><s>A</s></button>',
+          action: (e) => this.onButtonAction(e, 'strikethrough', false, null)
+        },
+        {
+          key: 'anchor',
+          html: '<button class="btn btn-default">LINK</button>',
+          action: (e) => this.onButtonAction(e, 'anchor', false, null)
+        },
+        {
+          key: 'orderedlist',
+          html: '<button class="btn btn-default">OL</button>',
+          action: (e) => this.onButtonAction(e, 'orderedlist', false, null)
+        },
+        {
+          key: 'unorderedlist',
+          html: '<button class="btn btn-default">UL</button>',
+          action: (e) => this.onButtonAction(e, 'unorderedlist', false, null)
+        }
+      ]
     };
 
-    Object.keys(baseOptions).forEach((k) => {
-      this.options[k] = options[k] ? options[k] : baseOptions[k];
-    })
+    // Extend default.
+    this.options = extend(baseOptions, options);
   }
 
   createToolbar() {
-
     if (this.tbar) { return; }
     this.tbar = document.createElement('div');
-    let buttons = [
-      {
-        key: 'h3',
-        html: '<div class="toolbar__button">h3</div>',
-        action: (e) => console.log('h3 action')
-      },
-      {
-        key: 'bold',
-        html: '<div class="toolbar__button"><b>B</b></div>',
-        action: (e) => this.onButtonAction('bold')
-      }
-    ]
-
-    for (var option of buttons) {
+    for (var option of this.options.buttons) {
       let button = this.createToolbarButton(option);
       this.tbar.appendChild(button);
     }
     this.editor.parentNode.insertBefore(this.tbar, this.editor);
   }
 
-  onButtonAction(e, ...args) {
-    e.preventDefault();
-    e.stopPropagation();
-    this.exec(...arg);
-  }
 
   createToolbarButton(options = null) {
     if (!options) {return;}
     let button = document.createElement('div')
+    button.className = 'tbar__btn'
     button.innerHTML = options.html;
-    button.addEventListener('click', options.action)
+    button.addEventListener('click', options.action);
+    button.addEventListener('mousedown', (e) => e.preventDefault()); //canceling contentEditable will blur.
     return button
   }
 
@@ -79,11 +126,14 @@ export class Editor {
       throw new Error("Missing editor");
     }
 
+
     this.editor.addEventListener('focus', (e) => this.onFocus(e));
     this.editor.addEventListener('change', (e) => this.onChange(e));
     this.editor.addEventListener('blur', (e) => this.onBlur(e));
     this.editor.addEventListener('mouseup', (e) => this.onMouseUp(e));
     this.editor.addEventListener('mousedown', (e) => this.onMouseDown(e));
+
+
   }
 
   reset() {
@@ -92,14 +142,34 @@ export class Editor {
     this.empty = true;
   }
 
+  dispatchEvent(eventName, ...args) {
+    let change = new Event(eventName);
+    this.editor.dispatchEvent(change);
+  }
+
   setValue(val) {
     this.value = val;
     this.editor.innerHTML =this.value;
     this.empty = false;
+    this.dispatchEvent('editor.change')
+  }
+
+  updateValue() {
+    this.value = this.editor.innerHTML;
+    this.dispatchEvent('editor.change')
   }
 
   exec(cmd, ...options) {
-    document.execCommand(cmd, options);
+    document.execCommand(cmd, ...options);
+    console.log(cmd, ...options);
+    this.updateValue();
+  }
+
+  onButtonAction(e, ...args) {
+    console.log('buttonAction', args);
+    e.preventDefault();
+    e.stopPropagation();
+    this.exec(...args);
   }
 
   onFocus(e) {
--- src/index.js
+++ src/index.js
@@ -2,3 +2,23 @@ import {Editor} from './editor/editor'
 
 let MyEditor = new Editor('editor');
 MyEditor.createRichEditor();
+
+class DebugCtrl {
+  constructor(target, editor) {
+    this.editor = editor;
+    this.element = document.getElementById(target);
+      this.createTextArea();
+
+    this.editor.editor.addEventListener('editor.change', (e) => {
+      this.textArea.innerHTML = this.editor.value;
+    });
+  }
+
+  createTextArea() {
+    this.textArea = document.createElement('textarea');
+    this.textArea.setAttribute('style', 'height:100%; width:100%;');
+    this.element.appendChild(this.textArea);
+  }
+}
+
+let EditorDebugger = new DebugCtrl('console', MyEditor);
